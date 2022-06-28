2f07ec58067a5b042a5cf92a7a84e98106784270
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-06-28 19:01:28 -0300

    auth screens

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,5 +1,16 @@
 export default {
 	name: "auth",
 	component: () => import("@/modules/auth/layouts/AuthLayout.vue"),
-	children: [],
+	children: [
+		{
+			path: "",
+			name: "login",
+			component: () => import("@/modules/auth/views/Login.vue"),
+		},
+		{
+			path: "signup",
+			name: "signup",
+			component: () => import("@/modules/auth/views/Signup.vue"),
+		},
+	],
 };
