bb3a0b88e9fc9c1e1221dc7a020b060470ba8be8
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-07-01 17:04:25 -0300

    .

--- src/api/journal.js
+++ src/api/journal.js
@@ -4,4 +4,12 @@ const apiJournal = axios.create({
 	baseURL: "https://vuejs-journal-default-rtdb.firebaseio.com",
 });
 
+apiJournal.interceptors.request.use((config) => {
+	config.params = {
+		auth: localStorage.getItem("idToken"),
+	};
+
+	return config;
+});
+
 export default apiJournal;
